6a38a484b0d779fbef87186ddaa0ad5a892b3479
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-06 07:13:04 -0300

    backend form

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const TicketForm = () => {
+  const router = useRouter();
+
   const handleChange = (e) => {
     const value = e.target.value;
     const name = e.target.name;
@@ -13,8 +15,20 @@ const TicketForm = () => {
     }));
   };
 
-  const handleSubmit = () => {
-    console.log("submitted");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/Tickets", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      "content-type": "application/json",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to create Ticket.");
+    }
+
+    router.refresh();
+    router.push("/");
   };
 
   const startingTicketData = {
